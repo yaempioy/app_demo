4b5880d14b7dbb6db5be712e10887ddcbdcb3b08
Kitti Boonchan <kornkittig@gmail.com>
2017-05-11 14:20:47 +0700

    Add dotenv file.

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+var dotenv = require('dotenv')
 var express = require('express');
 var app = express();
+dotenv.config({silent: true})
+dotenv.load()
 
+const PORT = process.env.PORT || 5000
 /**
  * Test Jenkine Webhook
  * Replace Jenkine the build command npm install to moche
  * */
 
-app.get('/',function(req,res) {
-  res.send("Hello Jenkins World Nivin Automate Test ######");
-});
+app.get('/', (req,res) => { res.send("Hello Jenkins World Nivin Automate Test ######") })
 
-app.listen(4000);
+app.listen(PORT, () => {  console.log(`Server started on ${PORT}`)})
